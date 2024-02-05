b79bf233e7093bad43cfe4661a1fe15a136bc44b
ElCatBlack <leo_valenzuela12@hotmail.com>
2024-02-05 19:35:51 -0300

    style

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,18 +1,40 @@
 import './footer.css'
 
+const redesData = [
+    {
+        icon: './icons/github.png',
+        link: 'https://github.com/DEOteam',
+    },
+    {
+        icon: './icons/x.png',
+        link: 's',
+    },
+    {
+        icon: './icons/linkedin.png',
+        link: 's',
+    }
+]
+
+const Redes = ({link, icon})=>{
+    return(
+        <a target='_blank' href={link}><img src={icon} alt="" /></a>
+    )
+}
+
 export function Footer(){
+
     return(
     <footer className="footer" id="footer">
-        <h1>footer</h1>
-        <section className='contact-conteiner'>
-
-            <div className='contact-content'>
-                <div className='contact-icon'><img src="" alt="" /></div>
-                <p></p>
+        <div className='content-footer'>
+            <div className='header-footer'>
+                <div className='logo-footer'><img src="./img/deo.png" alt="" /></div>
+                <div className='redes-footer'>
+                {redesData.map((red, index) => (
+                            <Redes key={index + 1} icon={red.icon} link={red.link} />
+                ))}    
+                </div>
             </div>
-            
-        </section>
-        <section></section>
+        </div>
     </footer>
     )
 }
